f1cb7318a64c855fca0e4434151fd3bf47453193
psyafter <psyafter@gmail.com>
2015-11-12 12:44:10 +0200

    kernel toolchain
    Change-Id: Iba97c76f0a761b3c66d12ca2fdd13070924de8a6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,12 +25,12 @@ TARGET_BOARD_INFO_FILE := device/samsung/i9100/board-info.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
 
 # Inline kernel building
-#TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-#TARGET_KERNEL_CONFIG := cyanogenmod_i9100_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
+TARGET_KERNEL_CONFIG := cyanogenmod_i9100_defconfig
 #TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412+
 #TARGET_KERNEL_CONFIG := cyanogenmod_i9100_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412-GeniSys
-TARGET_KERNEL_CONFIG := i9100_defconfig
+#TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412-GeniSys
+#TARGET_KERNEL_CONFIG := i9100_defconfig
 
 # Kernel Toolchain
 #KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
@@ -40,8 +40,11 @@ TARGET_KERNEL_CONFIG := i9100_defconfig
 #KERNEL_TOOLCHAIN_PREFIX := arm-linux-gnueabihf-
 #KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilt/$(HOST_OS)-x86/toolchain/linaro-4.9-14.06/bin/"
 #KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-sm/bin
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/kernel/samsung/smdk4412-GeniSys/toolchain/arm-cortex-a9/bin
+
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/kernel/samsung/smdk4412-GeniSys/toolchain/arm-cortex-a9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/device/samsung/i9100/prebuilt/toolchain/kernel/arm-cortex-a9//bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+TARGET_NO_RECOVERY := true
 
 # Rom Toolchain
 #TARGET_GCC_VERSION_EXP := 4.9-sm
